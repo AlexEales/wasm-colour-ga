ba0cd726e2092779656a44672c619302cf43d74d
alex.eales <alex.eales@potatolondon.com>
2019-12-05 09:52:55 +0000

    Added generation display functions

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,15 @@ const STOP_BUTTON = document.querySelector('#stop-button');
 const RESET_BUTTON = document.querySelector('#reset-button');
 
 const GENERATION_DISPLAY = document.querySelector('#generation-history-display');
+const GENERATION_DISPLAY_CARD_ATTR = 'generation-display-card';
+const GENERATION_DISPLAY_PLACEHOLDER = `
+<div generation-display-card>
+    <div>
+        <h3>No results</h3>
+        <p>Input your parameters and press start to begin!</p>
+    </div>
+</div>
+`;
 
 // FUNCTIONS
 const updateColourDisplay = () => {
@@ -33,6 +42,32 @@ const updateColourDisplay = () => {
     }
     FORM_ELEMENTS['TARGET_COLOUR'].display.style.backgroundColor = colour;
 };
+
+const createGenerationDisplayCard = (generation, colour, score) => `
+<div ${GENERATION_DISPLAY_CARD_ATTR}>
+    <div class="flex">
+        <div class="w-12 h-12 rounded mr-4" style="background-color: #${colour}"></div>
+        <div>
+            <h3 class="font-bold capitalize">generation ${generation}</h3>
+            <p class="uppercase text-gray-500">#${colour}</p>
+        </div>
+    </div>
+    <p class="font-bold capitalize text-2xl text-gray-900">${score}</p>
+</div>
+`;
+
+const clearGenerationDisplay = () => {
+    // Remove all elements with the generation display card attribute and add the placeholder
+    GENERATION_DISPLAY.querySelectorAll(`[${GENERATION_DISPLAY_CARD_ATTR}]`).forEach(
+        elem => elem.parentNode.removeChild(elem)
+    );
+};
+
+const resetGenerationDisplay = () => {
+    // Clear the display and add the placeholder element
+    clearGenerationDisplay();
+    GENERATION_DISPLAY.insertAdjacentHTML('beforeend', GENERATION_DISPLAY_PLACEHOLDER);
+}
 
 // EVENT LISTENERS
 FORM_ELEMENTS['TARGET_COLOUR'].input.addEventListener('change', updateColourDisplay);
@@ -60,9 +95,11 @@ console.log(simulation);
 let result = simulation.simulate_generation();
 console.log(result.get_value());
 console.log(result.get_score());
+console.log(simulation.get_population_snapshot(0, 1000));
 
 for (let i = 0; i < 50; i++) {
     result = simulation.simulate_generation();
-    console.log(result.get_value());
-    console.log(result.get_score());
 }
+console.log(result.get_value());
+console.log(result.get_score());
+console.log(simulation.get_population_snapshot(49, 1000));
